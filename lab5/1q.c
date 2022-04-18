88e2b82d2c063fdc000fb82076086362399af9ed
GaneshN29 <77326035+GaneshN29@users.noreply.github.com>
2022-04-18 11:18:13 +0530

    Add files via upload

---
+++ lab5/1q.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <sys/types.h> 
+#include <sys/wait.h> 
+#include <unistd.h>  
+int main(void)
+{
+    pid_t pid;
+    if(fork()==0){
+        printf(" |-> Inside child \n");
+        printf(" |-> Pid of child: %d \n", getpid());
+        printf(" |-> PPid of child: %d \n", getppid());
+        printf(" |-> Child Terminated \n\n");
+        return 1;
+    }
+    else if(fork>0){
+        printf("Inside parent \n");
+        pid = wait(NULL);
+        printf("Pid of Parent: %d \n", getpid());
+        printf("PPid of Parent: %d \n", getppid());
+        printf("Parent Terminated \n");
+    }
+    else
+    {
+        printf("Fork Failed");
+    }
+}
---
+++ lab5/example.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <sys/types.h> 
+#include <sys/wait.h> 
+#include <unistd.h>  
+int main()
+{
+printf("hello");
+}
